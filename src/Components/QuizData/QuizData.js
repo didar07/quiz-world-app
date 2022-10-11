45434b15677c3b916e2406c556df2519823caf07
didar07 <didarul111015@gmail.com>
2022-10-11 20:39:53 +0600

    eye icon added

--- src/Components/QuizData/QuizData.js
+++ src/Components/QuizData/QuizData.js
@@ -1,26 +1,39 @@
+import { faEye } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import Option from '../Option/Option';
 import './QuizData.css'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const QuizData = ({ question, id, options }) => {
     // console.log(correctAnswer)
     const { correctAnswer } = question
     console.log(correctAnswer)
 
-
+    const handleEye = () => {
+        toast(correctAnswer)
+    }
 
     return (
         <div>
             <div className='question-container'>
-                <h1 className='text-2xl font-bold'>{question.question}</h1>
                 <div className='answer'>
+                    <h1 className='text-2xl font-bold'>{question.question}</h1>
 
                     {
                         question.options.map(option => <Option key={option.id} option={option} correctAnswer={correctAnswer}></Option>)
                     }
 
                 </div>
+                <div className='eye-container'>
+                    <button onClick={handleEye}><FontAwesomeIcon icon={faEye}></FontAwesomeIcon></button>
+                </div>
             </div>
+
+            <ToastContainer></ToastContainer>
         </div>
+
     );
 };
 
